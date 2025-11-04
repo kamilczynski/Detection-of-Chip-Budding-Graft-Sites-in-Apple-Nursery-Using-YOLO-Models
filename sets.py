2daa635af5b14e77f820514025505eb9886be316
kamilczynski <kamilczynski1313@gmail.com>
2025-11-04 17:52:54 +0100

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,27 @@
+{"name": "YOLOv8s_RGB8S", "path": r"C:\Users\HARDPC\runs\train_RGB8S\yolov8s_RGB8S\weights\best.pt", "map5095": 0.975},
+    {"name": "YOLOv11s_RGBG11S", "path": r"C:\Users\HARDPC\runs\train_RGB11S\yolov11s_RGB11S\weights\best.pt", "map5095": 0.975},
+    {"name": "YOLOv12s_RGBG12S", "path": r"C:\Users\HARDPC\runs\train_RGB12S\yolov12s_RGB12S\weights\best.pt", "map5095": 0.973},
+    {"name": "YOLOv8s_NIR8S", "path": r"C:\Users\HARDPC\runs\train_NIR8S\yolov8s_NIR8S\weights\best.pt", "map5095": 0.984},
+    {"name": "YOLOv11s_NIR11S", "path": r"C:\Users\HARDPC\runs\train_NIR11S\yolov11s_NIR11S\weights\best.pt", "map5095": 0.986},
+    {"name": "YOLOv12s_NIR12S", "path": r"C:\Users\HARDPC\runs\train_NIR12S\yolov12s_NIR12S\weights\best.pt", "map5095": 0.986},
+    {"name": "YOLOv8s_RE8S", "path": r"C:\Users\HARDPC\runs\train_RE8S\yolov8s_RE8S\weights\best.pt", "map5095": 0.985},
+    {"name": "YOLOv11s_RE11S", "path": r"C:\Users\HARDPC\runs\train_RE11S\yolov11s_RE11S\weights\best.pt", "map5095": 0.985},
+    {"name": "YOLOv12s_RE12S", "path": r"C:\Users\HARDPC\runs\train_RE12S\yolov12s_RE12S\weights\best.pt", "map5095": 0.983},
+    {"name": "YOLOv8s_R8S", "path": r"C:\Users\HARDPC\runs\train_R8S\yolov8s_R8S\weights\best.pt", "map5095": 0.941},
+    {"name": "YOLOv11s_R11S", "path": r"C:\Users\HARDPC\runs\train_R11S\yolov11s_R11S\weights\best.pt", "map5095": 0.949},
+    {"name": "YOLOv12s_R12S", "path": r"C:\Users\HARDPC\runs\train_R12S\yolov12s_R12S\weights\best.pt", "map5095": 0.952},
+    {"name": "YOLOv8s_G8S", "path": r"C:\Users\HARDPC\runs\train_G8S\yolov8s_G8S\weights\best.pt", "map5095": 0.973},
+    {"name": "YOLOv11s_G11S", "path": r"C:\Users\HARDPC\runs\train_G11S\yolov11s_G11S\weights\best.pt", "map5095": 0.974},
+    {"name": "YOLOv12s_G12S", "path": r"C:\Users\HARDPC\runs\train_G12S\yolov12s_G12S\weights\best.pt", "map5095": 0.972},
+
+
+
+
+ {"name": "YOLOv8s_oczkodetekcja8S",   "path": r"C:\Users\HARDPC\runs\train_oczkodetekcja8S\yolov8s_oczkodetekcja8S\weights\best.pt",  "map5095": 0.695},
+    {"name": "YOLOv9s_oczkodetekcja9S",   "path": r"C:\Users\HARDPC\runs\train_oczkodetekcja9S\yolov9s_oczkodetekcja9S\weights\best.pt",  "map5095": 0.693},
+    {"name": "YOLOv10s_oczkodetekcja10S",   "path": r"C:\Users\HARDPC\runs\train_oczkodetekcja10S\yolov10s_oczkodetekcja10S\weights\best.pt",  "map5095": 0.691},
+    {"name": "YOLOv11s_oczkodetekcja11S",   "path": r"C:\Users\HARDPC\runs\train_oczkodetekcja11S\yolov11s_oczkodetekcja11S\weights\best.pt",  "map5095": 0.701},
+    {"name": "YOLOv12s_oczkodetekcja12S",   "path": r"C:\Users\HARDPC\runs\train_oczkodetekcja12S\yolov12s_oczkodetekcja12S\weights\best.pt",  "map5095": 0.706},
+
+    {"name": "YOLOv8sseg_oczkosegmentacja8S",   "path": r"C:\Users\HARDPC\runs\train_oczkosegmentacja8S\yolov8s_oczkosegmentacja8S\weights\best.pt",  "map5095": 0.683},
+    {"name": "YOLOv11seg_oczkosegmentacja9S",   "path": r"C:\Users\HARDPC\runs\train_oczkosegmentacja11S\yolov11s_oczkosegmentacja11S\weights\best.pt",  "map5095": 0.679},
